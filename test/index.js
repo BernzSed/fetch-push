77f963d9c80c823beaa07fb4e159fbe59fe0390d
Bernie Stern <bernzs@gmail.com>
2017-10-13 21:14:42 -0400

    npm init, initial stuff

---
+++ test/index.js
@@ -0,0 +1,10 @@
+import chai, { assert, expect } from 'chai';
+import sinonChai from 'sinon-chai';
+import sinon from 'sinon';
+import buildFetch from '../src';
+
+chai.use(sinonChai);
+
+describe('fetch-push', () => {
+  it.skip('will be build', () => {});
+});
