3a889d51efcc4ad5a083946e096d9061b7733010
Bernie Stern <bernzs@gmail.com>
2017-10-16 17:46:08 -0400

    more stuff. Still doesn't work; may need to use an alpha version of node-fetch

---
+++ src/axios-converters.js
@@ -0,0 +1,37 @@
+import { Response } from 'node-fetch';
+
+function getAxiosRequestHeaders(request) {
+  const headersObj = {};
+  for (const [name, value] of request.headers) { // eslint-disable-line no-restricted-syntax
+    headersObj[name] = value;
+  }
+  // TODO request.cache
+  return headersObj;
+}
+
+export function convertToAxiosRequest(request) {
+  return {
+    url: request.url,
+    method: request.method,
+    headers: getAxiosRequestHeaders(request),
+
+    // Not supporting request body, because this will only be for GET.
+
+    // `withCredentials` indicates whether or not cross-site Access-Control requests
+    // should be made using credentials
+    withCredentials: request.credentials === 'include',
+    // TODO FederatedCredential/PasswordCredential
+    // Or just wait until I replace axios-push with node-fetch.
+
+    responseType: 'stream'
+
+    // TODO cancelToken -> request.signal
+  };
+}
+
+export function convertFromAxiosResponse(response) {
+  // TODO eventually want to give devs the ability to chain requests
+  // on the server-side, so will need to return data. Not sure if this will be
+  // before or after I get rid of the axios-push dependency.
+  return new Response();
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import { Request } from 'node-fetch';
 import prepareAxios from 'axios-push';
+import {
+  convertToAxiosRequest,
+  convertFromAxiosResponse
+} from './axios-converters';
 
 export default function buildFetch(pageResponse) {
 
   const axios = prepareAxios(pageResponse);
   // axios-push is hopefully temporary,
   // but it's necessary right now because node-fetch doesn't
-  // yet use AbortSignal
+  // yet use AbortSignal.
 
 
   return function fetchPush(input, init) {
     const request = new Request(input, init);
+    const axiosRequestConfig = convertToAxiosRequest(request);
+    const axiosPromise = axios.request(axiosRequestConfig);
 
-    // TODO
+    const responsePromise = axiosPromise.then(convertFromAxiosResponse);
+    responsePromise.chain = function chain(...args) {
+      // TODO if this method is called, then don't return an empty promise;
+      // hold off on sending the push response so that another push_promise
+      // can be sent.
+      return responsePromise.then(...args);
+    };
+    return responsePromise;
   };
 }
